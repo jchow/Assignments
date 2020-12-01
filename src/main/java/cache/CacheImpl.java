78dcf648208bf242d48f12060c31b8f109dc2351
Jeff Chow <jeffyuchow@gmail.com>
2020-12-01 23:55:44 +0800

    Changes according to requirements

--- src/main/java/cache/CacheImpl.java
+++ src/main/java/cache/CacheImpl.java
@@ -3,24 +3,30 @@ package cache;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
 
 public class CacheImpl<K, V> implements Cache<K, V> {
     private final ConcurrentHashMap<K, V> itemStore = new ConcurrentHashMap<K, V>();
-    private final Set<K> keySet = new HashSet<>();
     private final Function<K, V> customFct;
+    private final ReentrantLock lockMe = new ReentrantLock();
 
     public CacheImpl(Function<K, V> customFct) {
         this.customFct = customFct;
     }
 
+    @Override
     public V get(K key) {
         if (key == null){
             return null;
         }
-        if (!itemStore.containsKey(key)) {
-            itemStore.putIfAbsent(key, invokeFunc(key));
+        lockMe.lock();
+        try {
+            if (!itemStore.containsKey(key)) {
+                itemStore.putIfAbsent(key, invokeFunc(key));
+            }
+        } finally {
+            lockMe.unlock();
         }
         return itemStore.get(key);
     }
--- src/test/java/cache/CacheImplTest.java
+++ src/test/java/cache/CacheImplTest.java
@@ -3,6 +3,10 @@ package cache;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -24,6 +28,28 @@ class CacheImplTest {
     void nullKeyCase() {
         assertNull(spyTarget.get(null));
         verify(spyTarget, times(0)).invokeFunc(any());
+    }
+
+    @Test
+    void exceptional(){
+        final CacheImpl<Integer, String> cache = new CacheImpl<Integer, String>(x-> {throw new RuntimeException("Fault");});
+        assertThrows(RuntimeException.class, () -> {cache.get(3);});
+    }
+
+    @Test
+    void multiThread() throws InterruptedException {
+        int numThreads = 2;
+        final int testKey = 100;
+        ExecutorService service = Executors.newFixedThreadPool(numThreads);
+        CountDownLatch count = new CountDownLatch(numThreads);
+        for(int i=0; i<numThreads; i++){
+            service.submit(()->{
+                    spyTarget.get(testKey);
+                    count.countDown();
+            });
+        }
+        count.await();
+        verify(spyTarget, times(1)).invokeFunc(testKey);
     }
 
 
